064193646135aa5ee96c73ed220a11e6912d223b
milenamarchese <mmarchese@fi.uba.ar>
2019-10-22 04:30:02 -0300

    simple circuit generator

---
+++ server/circuit_generator.h
@@ -0,0 +1,15 @@
+#ifndef TALLER_MICROMACHINES_CIRCUIT_GENERATOR_H
+#define TALLER_MICROMACHINES_CIRCUIT_GENERATOR_H
+
+
+#include <vector>
+
+//Clockwise circuit
+
+class CircuitGenerator {
+public:
+  static void generate_circuit();
+};
+
+
+#endif //TALLER_MICROMACHINES_CIRCUIT_GENERATOR_H
