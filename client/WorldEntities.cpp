6cb4b8b48a20007df9cfd81802932613cc18415a
pastine <casimiropastine@gmail.com>
2019-11-23 14:32:55 -0300

    saco corazones del medio

--- client/WorldEntities.cpp
+++ client/WorldEntities.cpp
@@ -44,10 +44,6 @@ void WorldEntities::render(Camera &camera) {
 													 std::get<1>(road_pos),
 													 std::get<2>(road_pos));
 		}
-	for (const std::tuple<float, float, float> &road_pos : straight_road_positions) {
-		camera.render_object(this->heart, std::get<0>(road_pos),
-												 std::get<1>(road_pos));
-	}
 
     _render_simple_object(this->boulder_positions, this->boulder, camera);
     _render_simple_object(this->oil_positions, this->oil, camera);
